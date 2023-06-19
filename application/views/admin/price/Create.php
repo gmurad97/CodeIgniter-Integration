8ca6e14f91c8a4349a4135a2077d76216cdfc1de
gmurad97 <murad.dev@bk.ru>
2023-06-19 05:59:28 +0400

    Create.php Price (finished and bug fixed dynamic input)

--- application/views/admin/price/Create.php
+++ application/views/admin/price/Create.php
@@ -32,129 +32,115 @@
                 background-color: rgba(178, 190, 181, 0.3);
             }
 
-            .fa-dinamic-price-del {
+            .fa-dynamic-price-del {
                 font-size: 32px;
                 transition: color 128ms linear;
                 cursor: pointer;
             }
 
-            .fa-dinamic-price-del:hover {
+            .fa-dynamic-price-del:hover {
                 color: rgba(142, 39, 31, 1) !important;
             }
 
-            .fa-dinamic-price-add {
+            .fa-dynamic-price-add {
                 font-size: 32px;
                 transition: color 128ms linear;
                 cursor: pointer;
             }
 
-            .fa-dinamic-price-add:hover {
+            .fa-dynamic-price-add:hover {
                 color: rgba(0, 106, 78, 1) !important;
             }
         </style>
         <form>
             <h3 class="bg-gradient-dark text-center text-white mb-3 py-2 rounded"><b>Price</b></h3>
             <div class="row">
-                <div class="col-sm-8" id="dynamic-parent-price">
+                <div class="col-sm-8">
                     <div class="form-group">
                         <label for="price_base_h1_text"><b>Base H1 text</b></label>
                         <input name="input_price_base_h1_text" id="price_base_h1_text" type="text" class="form-control">
                     </div>
-
-
-
-                    <div class="row" id="dynamic-row-price">
-                        <div class="col-sm-5">
-                            <div class="form-group">
-                                <label for="price_text_one"><b>Price text</b></label>
-                                <input name="input_price_text_one" id="price_text_one" type="text" class="form-control">
+                    <div class="form-group d-flex flex-column-reverse" id="dynamic-parent-price">
+                        <div class="row" id="dynamic-row-price">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label for="price_text"><b>Price text</b></label>
+                                    <input name="input_price_text" id="price_text" type="text" class="form-control">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="form-group">
-                                <label for="price_value_one"><b>Price value</b></label>
-                                <input name="input_price_value_one" id="price_value_one" type="number" class="form-control">
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <label for="price_value"><b>Price value</b></label>
+                                    <input name="input_price_value" id="price_value" type="number" class="form-control">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-sm-2">
-                            <div class="form-group">
-                                <label for="currency_one"><b>Currency</b></label>
-                                <select name="input_currency_one" id="currency_one" class="form-control">
-                                    <option value="azn">AZN</option>
-                                    <option value="eur">EUR</option>
-                                    <option value="usd">USD</option>
-                                    <option value="rub">RUB</option>
-                                </select>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    <label for="currency"><b>Currency</b></label>
+                                    <select name="input_currency" id="currency" class="form-control">
+                                        <option value="azn">AZN</option>
+                                        <option value="eur">EUR</option>
+                                        <option value="usd">USD</option>
+                                        <option value="rub">RUB</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-sm-1">
-                            <div class="form-group d-flex flex-column">
-                                <label for="currency_one"><b>Add</b></label>
-                                <i class="fas fa-plus-circle fa-dinamic-price-add text-success"></i>
+                            <div class="col-sm-1">
+                                <div class="form-group d-flex flex-column">
+                                    <label><b>Add</b></label>
+                                    <i class="fas fa-plus-circle fa-dynamic-price-add text-success"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    <!--                         
-                        <div class="col-sm-1">
-                            <div class="form-group d-flex flex-column">
-                                <label for="currency_one"><b>Del</b></label>
-                                <i class="fas fa-minus-circle fa-dinamic-price-del text-danger"></i>
-                            </div>
-                        </div>
-                    -->
-
+                    <h1 class="h6 text-muted text-center" id="dynamic-input-counter">Input Count: 1</h1>
                     <script>
-                        let dynamic_counter = 1;
+                        const MAX_DYNAMIC_INPUT_VALUE = 10;
+                        const dynamicPriceAdd = document.querySelector(".fa-dynamic-price-add");
+                        const dynamicParentPrice = document.querySelector("#dynamic-parent-price");
+                        const dynamicStatCounter = document.querySelector("#dynamic-input-counter");
 
-                        let cloned_dynamic_row = 
-                        `<div class="row" id="dynamic-row-price-${dynamic_counter}">
-                        <div class="col-sm-5">
-                            <div class="form-group">
-                                <label for="price_text_one"><b>Price text</b></label>
-                                <input name="input_price_text_one" id="price_text_one" type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="form-group">
-                                <label for="price_value_one"><b>Price value</b></label>
-                                <input name="input_price_value_one" id="price_value_one" type="number" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-sm-2">
-                            <div class="form-group">
-                                <label for="currency_one"><b>Currency</b></label>
-                                <select name="input_currency_one" id="currency_one" class="form-control">
-                                    <option value="azn">AZN</option>
-                                    <option value="eur">EUR</option>
-                                    <option value="usd">USD</option>
-                                    <option value="rub">RUB</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-sm-1">
-                        <div class="form-group d-flex flex-column">
-                                <label for="currency_one"><b>Del</b></label>
-                                <i class="fas fa-minus-circle fa-dinamic-price-del text-danger"></i>
-                            </div>
-                        </div>
-                    </div>`;
+                        function generateUID(uidLength) {
+                            let generatedUID = "";
+                            let wordsUID = "GcByxS1oRAnThCa7K2OqEplsIbPvMNX84mtwW6FZH9erdJu5L0kDjQYif3gUVz";
+                            for (let x = 0; x <= uidLength; x++) {
+                                generatedUID += wordsUID.charAt(Math.floor(Math.random() * wordsUID.length));
+                            }
+                            return generatedUID;
+                        }
+
+                        function deleteDI(elementID) {
+                            document.querySelector(`#${elementID}`).remove();
+                            dynamicStatCounter.innerText = "Input Count: " + (document.querySelectorAll("[id^='dynamic-row-price']").length);
+                        }
+
+                        dynamicPriceAdd.addEventListener("click", function() {
+                            dynamicPriceAdd.scrollIntoView();
+                            let generateDynamicUID = generateUID(8);
+                            if (!(document.querySelectorAll("[id^='dynamic-row-price']").length >= MAX_DYNAMIC_INPUT_VALUE)) {
+                                dynamicStatCounter.innerText = "Input Count: " + (document.querySelectorAll("[id^='dynamic-row-price']").length + 1);
+                                let newDynamicObject = document.createElement("div");
+                                newDynamicObject.classList.add("row");
+                                newDynamicObject.id = `dynamic-row-price-${generateDynamicUID}`;
+                                newDynamicObject.innerHTML += `<div class="col-sm-6"><div class="form-group"><label for="price_text_${generateDynamicUID}"><b>Price text</b></label><input name="input_price_text_${generateDynamicUID}" id="price_text_${generateDynamicUID}" type="text" class="form-control"></div></div><div class="col-sm-3"><div class="form-group"><label for="price_value_${generateDynamicUID}"><b>Price value</b></label><input name="input_price_value_${generateDynamicUID}" id="price_value_${generateDynamicUID}" type="number" class="form-control"></div></div><div class="col-sm-2"><div class="form-group"><label for="currency_${generateDynamicUID}"><b>Currency</b></label><select name="input_currency_${generateDynamicUID}" id="currency_${generateDynamicUID}" class="form-control"><option value="azn">AZN</option><option value="eur">EUR</option><option value="usd">USD</option><option value="rub">RUB</option></select></div></div><div class="col-sm-1"><div class="form-group d-flex flex-column"><label><b>Del</b></label><i class="fas fa-minus-circle fa-dynamic-price-del text-danger" onclick="deleteDI('dynamic-row-price-${generateDynamicUID}')"></i></div></div>`;
+                                dynamicParentPrice.appendChild(newDynamicObject);
+                            }
+                        });
                     </script>
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
-                        <script>
-                            function setData(input) {
-                                document.querySelector(".file_upload_block").style.backgroundImage = `url("${URL.createObjectURL(input.files[0])}")`;
-                                document.querySelector(".file_upload_block").style.color = "#ffffff";
-                            }
-                        </script>
                         <label for="price_lending_img"><b>Image Upload</b></label>
-                        <input type="file" name="input_price_lending_img" id="price_lending_img" onchange="setData(this);" hidden>
+                        <input type="file" name="input_price_lending_img" id="price_lending_img" hidden>
                         <div class="file_upload_block">
-                            <i class="fas fa-plus-circle"></i>
+                            <i class="fas fa-plus-circle fas-js"></i>
                         </div>
                         <script>
+                            document.querySelector("#price_lending_img").addEventListener("change", function() {
+                                document.querySelector(".fas-js").classList.replace("fa-plus-circle", "fa-pen");
+                                document.querySelector(".file_upload_block").style.backgroundImage = `url("${URL.createObjectURL(document.querySelector("#price_lending_img").files[0])}")`;
+                                document.querySelector(".file_upload_block").style.color = "#ffffff";
+                            });
                             document.querySelector(".file_upload_block").addEventListener("click", function() {
                                 document.querySelector("#price_lending_img").click();
                             });
